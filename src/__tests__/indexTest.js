1bdc525bee883488f423d616a933a5061d1ab409
Alexander2327 <danilov.aa1@mail.ru>
2022-12-18 22:26:46 +0300

    add func getSpecial

---
+++ src/__tests__/indexTest.js
@@ -0,0 +1,56 @@
+/* eslint-disable linebreak-style */
+import getSpetial from '../index';
+
+test('test function getSpetial', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+  const result = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+  const res = getSpetial(character);
+  expect(res).toEqual(result);
+});
+
+test('test function getSpetial without special', () => {
+  const character = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+  };
+  const result = [];
+  const res = getSpetial(character);
+  expect(res).toEqual(result);
+});
